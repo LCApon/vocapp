5f6bc54f0a5e5fbdb78ddc8cf66bc2984d6146fd
Luc Apon <lucaponsp+git@gmail.com>
2026-04-19 22:42:00 +0200

    Added jinja to project and CORS allow all origins

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 from database.model import Base
 from database.session import engine
@@ -20,6 +22,15 @@ vocapp = FastAPI(
     description="Self-hosted Language Learning App",
     version="0.0.1",
     lifespan=lifespan
+)
+
+vocapp.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    # allow_origins=["http://localhost:8000", "vocab_app_simple.html:1"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["*"],
 )
 
 # Basic setups
