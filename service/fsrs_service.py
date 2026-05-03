7cbf0e363dec54fa71f58f901b62b5eb09649de6
Luc Apon <lucaponsp+git@gmail.com>
2026-05-03 23:01:04 +0200

    Rescheduling functionality in fsrs_service

--- service/fsrs_service.py
+++ service/fsrs_service.py
@@ -1,12 +1,17 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, List
 if TYPE_CHECKING:
-    from database.model import Review
+    from database.model import Review, ReviewLog
 
-from fsrs import Scheduler, Card, Rating, State
+from datetime import timedelta
+from fsrs import Scheduler, Card, Rating, State, ReviewLog as ReviewLogFsrs
 
 # One scheduler instance, shared across the app
-scheduler = Scheduler()
+scheduler = Scheduler(
+    desired_retention=.92,
+    learning_steps=(timedelta(minutes=10), timedelta(hours=1)),
+    relearning_steps=(timedelta(hours=1), timedelta(days=1)),
+)
 
 def convert_to_fsrs_card(review: Review) -> Card:
     """Reconstruct an fsrs.Card from the fields stored in the DB."""
@@ -35,5 +40,16 @@ def get_updated_card(
         fsrs_rating,
         review_datetime=review.dt_last_review
     )
+
+    return updated_card
+
+def get_rescheduled_card(
+    review: Review,
+    lstReviewLog: List[ReviewLog]
+) -> Card:
+    fsrs_card = convert_to_fsrs_card(review)
+
+    lstFsrsReviewLog = [ReviewLogFsrs(fsrs_card.card_id, Rating(row.rating), row.dt_review, None) for row in lstReviewLog]
+    updated_card = scheduler.reschedule_card(fsrs_card, lstFsrsReviewLog)
 
     return updated_card
